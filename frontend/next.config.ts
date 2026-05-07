475535e2e19a48773780b359c7879c9433ac6e41
FaizSyed99 <whodisnigg77@gmail.com>
2026-05-08 01:28:49 +0530

    feat: initialize next.config.ts with dev origin and remote image patterns

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.56.1"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
